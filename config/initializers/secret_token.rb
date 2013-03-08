548f8393ccd00d7af3626243f7df8ba2db4765cc
Brian Mann <brian@osysit.com>
2013-03-08 18:56:08 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+JsBistro::Application.config.secret_token = '49a312d35492e9915f229594476eab6e00f2fb1aff48c2cef2d9a246294eb2d1fe05f5ee9f4f3c3db409a8a88a757b04ebba93d4eae58135e0c8408eef828163'
