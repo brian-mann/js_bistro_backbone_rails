0a3eed92e1b9aeef78a0dc2b957b7e89801340a7
Brian Mann <brian@osysit.com>
2013-03-08 20:14:00 -0500

    added numericality validation to party

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,5 +1,6 @@
 class Reservation < ActiveRecord::Base
   attr_accessible :confirmation_id, :name, :number, :party
 
-	validates_presence_of :name, :number, :party
+	validates_presence_of :name, :number, :party	
+	validates_numericality_of :party
 end
